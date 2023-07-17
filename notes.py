3c5695080d917c11e44942d6b36274522603ef90
Thouseef <thousithouseef731@gmail.com>
2023-07-17 20:26:31 +0530

    practice

---
+++ notes.py
@@ -0,0 +1,149 @@
+# Where we can use Python: =======================================================>
+"""
+1. For developing Desktop Applications
+2. For developing web Applications
+3. For developing database Applications
+4. For Network Programming
+5. For developing games
+6. For Data Analysis Applications
+7. For Machine Learning
+8. For developing Artificial Intelligence Applications
+9. For IOT
+...
+"""
+# Features of Python: =============================================================>
+"""
+1. Simple and easy to learn
+2. Freeware and Open Source
+3. High Level Programming language
+4. Platform Independent : -  PVM is responsible to convert into machine understandable form.
+5. Portability
+6. Dynamically Typed
+7. Both Procedure Oriented and Object Oriented
+8. Interpreted
+9. Extensible
+10. Embedded
+11. Extensive Library
+"""
+# Flavors of Python: ============================================================>
+"""
+1.CPython:
+     It is the standard flavor of Python. It can be used to work with C lanugage Applications
+     
+2. Jython or JPython:
+     It is for Java Applications. It can run on JVM
+     
+3. IronPython:
+     It is for C#.Net platform
+     
+4.PyPy:
+     The main advantage of PyPy is performance will be improved because JIT compiler is 
+     available inside PVM.
+     
+5.RubyPython
+     For Ruby Platforms
+     
+6. AnacondaPython
+     It is specially designed for handling large volume of data processing
+"""
+
+# Identifiers ===============================================================================>
+"""
+A name in Python program is called identifier.
+It can be class name or function name or module name or variable name
+
+Note:
+1. If identifier starts with _ symbol then it indicates that it is private
+2. If identifier starts with __(two under score symbols) indicating that strongly private 
+identifier.
+3.If the identifier starts and ends with two underscore symbols then the identifier is 
+language defined special name,which is also known as magic methods.
+"""
+
+# Reserved Words: ===========================================================================>
+"""
+In Python some words are reserved to represent some meaning or functionality. Such type 
+of words are called Reserved words
+
+There are 33 reserved words available in Python.
+                True,False,None
+                and, or ,not,is
+                if,elif,else
+                while,for,break,continue,return,in,yield
+                try,except,finally,raise,assert
+                import,from,as,class,def,pass,global,nonlocal,lambda,del,with
+"""
+
+# Type Casting: ==============================================================================>
+"""
+We can convert one type value to another type. This conversion is called Typecasting or 
+Type coersion.
+1. int()
+2. float()
+3. complex()
+4. bool()
+5. str()
+"""
+# Escape Characters: ===========================================================================>
+"""
+1) \n==>New Line
+2) \t===>Horizontal tab
+3) \r ==>Carriage Return
+4) \b===>Back space
+5) \f===>Form Feed
+6) \v==>Vertical tab
+7) \'===>Single quote
+8) \"===>Double quote
+9) \\===>back slash symbo
+"""
+
+# Operators: =====================================================================================>
+"""
+1. Arithmetic Operators: +,-,*,/,%,//,**
+2. Comparison Operators: >,>=,<,<=
+   Equality operators: == , !=
+3. Logical Operators: and, or ,not
+4. Bitwise Operators: &,|,^,~,<<,>>
+5. Assignment Operators: +=,-=,*=,/=,%=,//=,**=,&=,|=,^=,>>=,<<=
+6. Identity Operators: is , is not
+7. Membership operators: in , not in 
+
+---------------------
+    PEMDAS
+--------------------
+"""
+
+# Ternary Operator: ==============================================================================>
+"""
+If condition is True then firstValue will be considered else secondValue will be considered.
+
+a,b=10,20 
+x=30 if a<b else 40 
+print(x) # 30
+"""
+
+# Mathematical Functions (math Module) ==========================================================>
+"""
+Module : - A Module is collection of functions, variables and classes etc
+
+import math
+
+=========================================== Important functions of math module:-
+
+ceil(x)
+floor(x)
+pow(x,y)
+factorial(x)
+trunc(x)
+gcd(x,y)
+sin(x)
+cos(x)
+tan(x)
+
+=========================================== Important variables of math module:
+pi3.14
+e===>2.71
+inf ==>infinity
+nan ==>not a number
+
+"""
