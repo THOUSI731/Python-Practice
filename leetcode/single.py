f1f1df9103eb72c2b9adde8e87a2adf6d9e0006b
Thouseef <thousithouseef731@gmail.com>
2023-09-26 20:54:07 +0530

    practice

---
+++ leetcode/single.py
@@ -0,0 +1,15 @@
+# class Solution:
+#     def removeDuplicateLetters(self,s) -> str:
+#         for i in range(len(s)):
+#              for j in range(len(s)):
+#                   if s[i] == s[j]:
+#                        pass
+#                   else:
+#                        print(s[i])
+   
+
+        
+# a=Solution()
+# print(a.removeDuplicateLetters("abcdeddg"))
+for i in range(1,11):
+     print("2x",i,"=",2*i) 
