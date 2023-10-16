9eb312cb9f769427efc8ee18cb03c5b4f13b845d
Thouseef <thousithouseef731@gmail.com>
2023-10-16 11:16:40 +0530

    13

---
+++ leetcode/13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self,s):
+          roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+          total = 0
+          i = 0
+          
+          while i < len(s):
+               if i < len(s) - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                    total += roman_to_int[s[i + 1]] - roman_to_int[s[i]]
+                    i += 2 
+               else:
+                    total += roman_to_int[s[i]]
+                    i += 1
+          
+          return total
+
+a=Solution()
+print(a.romanToInt("MCMXCIV"))
